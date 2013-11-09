9d77875a5f8ef10e72c88f9940376b786ca7055c
Tom Draken <tom.draken@zoho.com>
2013-11-09 20:09:24 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'c0323dbcb75cd934a1df2ec7e86c24077feccb89b770227682e737752a310b97d7e80fe16a172b55827791a6f4ac5c31e10d1072479b25c58611890c98a846bb'
